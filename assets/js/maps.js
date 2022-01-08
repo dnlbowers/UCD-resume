f1e2a891ef9f26d2d140aa035d4a239140278a46
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2022-01-08 23:51:25 +0100

    Make labels display and create seperate JS folder

---
+++ assets/js/maps.js
@@ -0,0 +1,36 @@
+function initMap() {
+    let map = new google.maps.Map(document.getElementById("map"), {
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
+        },
+        zoom: 3,
+    });
+
+    const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const locations = [{
+            lat: 40.785091,
+            lng: -73.968285
+        },
+        {
+            lat: 41.084045,
+            lng: -73.874245
+        },
+        {
+            lat: 40.754932,
+            lng: -73.984016
+        }
+    ];
+
+    const markers = locations.map(function(location, i){
+       return new google.maps.Marker({
+           position: location,
+           label: labels[i % labels.length]
+       }); 
+    });
+
+    const markerCluster = new markerClusterer.MarkerClusterer({
+        map,
+        markers
+    });
+};
